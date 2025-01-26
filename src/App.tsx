be79c8703cc5d5997f7851c87bf29ce80f1159f1
JakubGiedzicz <jakubgiedzicz@gmail.com>
2025-01-26 20:38:08 +0100

    linkgroup fixed
    mobile useLocation todo

--- src/App.tsx
+++ src/App.tsx
@@ -7,42 +7,56 @@ import {
   MantineProvider,
   Image,
   Center,
-  Flex} from "@mantine/core";
+  Flex,
+  Container,
+  Stack,
+} from "@mantine/core";
 import { Link, Outlet } from "react-router-dom";
 import { useDisclosure } from "@mantine/hooks";
 import LinkGroup from "./Components/LinkGroup";
 import Logo from "./assets/logo-preview.png";
 import "@mantine/core/styles.css";
-import styles from './App.module.css'
+import styles from "./App.module.css";
+import classes from "./Components/LinkGroup.module.css";
 
 function App() {
   const [opened, { toggle }] = useDisclosure();
+  const links = (mobile: boolean) => {
+    if (mobile) {
+      return <LinkGroup burger={false} />;
+    } else {
+      return <LinkGroup burger={true} />;
+    }
+  };
   return (
     <MantineProvider>
       <AppShell
         header={{ height: 200 }}
         navbar={{
           width: 300,
-          breakpoint: "sm",
+          breakpoint: "md",
           collapsed: { mobile: !opened, desktop: true },
         }}
       >
         <AppShell.Header>
-          <Group justify="space-around" h={'100%'}>
-              <Link to={"/"}>
-                <Image src={Logo} w={175} />
-              </Link>
-              <LinkGroup burger={true} />
-            <Burger
-              opened={opened}
-              onClick={toggle}
-              hiddenFrom="sm"
-              size="xl"
-            />
+          <Group justify="space-around" h={"100%"}>
+            <Link to={"/"}>
+              <Image src={Logo} w={175} />
+            </Link>
+            <header className={classes.header}>
+              <Container size="md">
+                <div className={classes.inner}>{links(true)}</div>
+                <Burger opened={opened} onClick={toggle} size="xl" hiddenFrom="md" />
+              </Container>
+            </header>
+
           </Group>
         </AppShell.Header>
-        <AppShell.Navbar pb={opened ? 48 : 0} className={opened ? styles.navbar_shadow : ''}>
-          {/* <LinkGroup burger={false} /> */}
+        <AppShell.Navbar
+          className={opened ? styles.navbar_shadow : ""}
+          hiddenFrom="md"
+        >
+          {links(false)}
         </AppShell.Navbar>
         <AppShell.Main>
           <Outlet />
--- src/Components/LinkGroup.tsx
+++ src/Components/LinkGroup.tsx
@@ -1,47 +1,75 @@
 import { NavLink as Link } from "react-router-dom";
-import { Burger, Center, Container, Grid, Group, Menu, NavLink, Stack } from "@mantine/core";
+import {
+  Burger,
+  Center,
+  Container,
+  Grid,
+  Group,
+  Menu,
+  NavLink,
+  Stack,
+} from "@mantine/core";
 import classes from "./LinkGroup.module.css";
 import { useDisclosure } from "@mantine/hooks";
-import { IconChevronDown } from '@tabler/icons-react';
+import { IconChevronDown, IconChevronRight } from "@tabler/icons-react";
 
 type Props = {
   burger: boolean;
 };
 const links = [
-  { link: '/o-mnie', label: 'O mnie' },
+  { link: "/o-mnie", label: "O mnie" },
   {
-    link: '/konsultacje',
-    label: 'Konsultacje indywidualne',
+    link: "konsultacje",
+    label: "Konsultacje indywidualne",
     links: [
-      { link: '/konsultacje-behawioralne', label: 'Konsultacje behawioralne' },
-      { link: '/konsultacje-dietetyczne', label: 'Konsultacje dietetyczne' }
+      { link: "/konsultacje-behawioralne", label: "Konsultacje behawioralne" },
+      { link: "/konsultacje-dietetyczne", label: "Konsultacje dietetyczne" },
     ],
   },
   {
-    link: '/zajecia-grupowe',
-    label: 'Zajęcia grupowe',
+    link: "/zajecia-grupowe",
+    label: "Zajęcia grupowe",
     links: [
-      { link: '/psieprzedszkole', label: 'Psieprzedszkole' },
-      { link: '/psiapodstawowka', label: 'Psiapodstawówka' }
+      { link: "/psieprzedszkole", label: "Psieprzedszkole" },
+      { link: "/psiapodstawowka", label: "Psiapodstawówka" },
     ],
   },
-  { link: '/cennik', label: 'Cennik' },
-  { link: '/kontakt', label: 'Kontakt' },
-  { link: '/blog', label: 'Blog' },
+  { link: "/cennik", label: "Cennik" },
+  { link: "/kontakt", label: "Kontakt" },
+  { link: "/blog", label: "Blog" },
 ];
 const LinkGroup = (props: Props) => {
   const [opened, { toggle }] = useDisclosure(false);
 
   const items = links.map((link) => {
     const menuItems = link.links?.map((item) => (
-      <Menu.Item key={item.link}><NavLink component={Link} to={item.link} label={item.label}/></Menu.Item>
+      <Menu.Item key={item.link}>
+        <NavLink
+          component={Link}
+          to={item.link}
+          label={item.label}
+          className={classes.link}
+        />
+      </Menu.Item>
     ));
-
     if (menuItems) {
       return (
-        <Menu key={link.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
+        <Menu
+          key={link.label}
+          trigger="hover"
+          transitionProps={{ exitDuration: 0 }}
+          withinPortal
+          position={props.burger ? "right" : "bottom"}
+        >
           <Menu.Target>
-          <NavLink key={link.label} component={Link} to={link.link} className={classes.link} label={link.label} rightSection={<IconChevronDown size={16} stroke={1.5}/>}/>
+            <NavLink
+              key={link.label}
+              component={Link}
+              to={link.link}
+              className={classes.link}
+              label={link.label}
+              rightSection={props.burger ? <IconChevronRight size={16} stroke={1.5} /> : <IconChevronDown size={16} stroke={1.5} />}
+            />
           </Menu.Target>
           <Menu.Dropdown>{menuItems}</Menu.Dropdown>
         </Menu>
@@ -49,22 +77,35 @@ const LinkGroup = (props: Props) => {
     }
 
     return (
-      <NavLink key={link.label} component={Link} to={link.link} className={classes.link} label={link.label}/>
+      <NavLink
+        key={link.label}
+        component={Link}
+        to={link.link}
+        className={classes.link}
+        label={link.label}
+      />
     );
   });
-
+  const mobile = (mobile: boolean) => {
+    if (!mobile) {
+      return (
+        <Group gap={5} visibleFrom="md" wrap="nowrap">
+          {items}
+        </Group>
+      );
+    } else {
+      return (
+        <Stack gap={32} hiddenFrom="md" h='100%' justify="center">
+          {items}
+        </Stack>
+      );
+    }
+  };
   return (
-    <header className={classes.header}>
-      <Container size="md">
-        <div className={classes.inner}>
-          <Group gap={5} visibleFrom="sm" wrap="nowrap">
-            {items}
-          </Group>
-          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
-        </div>
-      </Container>
-    </header>
+    <>
+      {mobile(props.burger)}
+    </>
   );
-}
+};
 
 export default LinkGroup;
